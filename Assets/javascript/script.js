73a155f02503fa45f55d1f53e20d3987af5af2f0
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-01-09 20:27:29 -0800

    final draft pending final check

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -10,7 +10,11 @@ var LOSSES = 0;
 
 // initialize game on key up of any key
 function initialize() {
-    $(window).keyup(function() {
+    $(window).keypress(function(e) { // prevent scroll down if space bar is hit
+        if (e.which === 32) {
+            e.preventDefault();
+        }
+
         RANDOMNUMBER = Math.floor(Math.random() * 102 + 19);
         CRYSTAL1 = Math.floor(Math.random() * 12 + 1);
         CRYSTAL2 = Math.floor(Math.random() * 12 + 1);
@@ -39,37 +43,47 @@ function play() {
     $("#crystal1").click(function() {
         SCORE = SCORE + CRYSTAL1;
         $("#your-score").text(SCORE);
+        winorlose();
         return;
     })
 
     $("#crystal2").click(function() {
         SCORE = SCORE + CRYSTAL2;
         $("#your-score").text(SCORE);
+        winorlose();
         return;
     })
 
     $("#crystal3").click(function() {
         SCORE = SCORE + CRYSTAL3;
         $("#your-score").text(SCORE);
+        winorlose();
         return;
     })
 
     $("#crystal4").click(function() {
         SCORE = SCORE + CRYSTAL4;
         $("#your-score").text(SCORE);
+        winorlose();
         return;
     })
+}
+
+play();
+
+// what happens when you win or lose
+function winorlose() {
     if ((SCORE === RANDOMNUMBER) && (RANDOMNUMBER > 0)) {
         console.log("win");
-        WINS = WINS++;
+        WINS = WINS+1; // why does WINS++ not work?
         $("#wins").text(WINS);
-        alert("Gneiss win, you rock! Press any key to play again.");
+        alert("Gneiss win, you rock star! Press any key to play again.");
         reset();
         return;
     }
     if (SCORE > RANDOMNUMBER) {
         console.log("lose");
-        LOSSES = LOSSES++;
+        LOSSES = LOSSES+1; // why does LOSSES++ not work?
         $("#losses").text(LOSSES);
         alert("You lose! But you're tufa than that, so of quartz you're gonna try again, right? Press any key to start over.");
         reset();
@@ -77,11 +91,13 @@ function play() {
     }
 }
 
-play();
-
 // reset everything except win/lose count after each round
 function reset() {
-    $(window).keyup(function() {
+    $(window).keyup(function(e) {
+        if (e.which === 32) {
+            e.preventDefault();
+        }
+
         RANDOMNUMBER = Math.floor(Math.random() * 102 + 19);
         CRYSTAL1 = Math.floor(Math.random() * 12 + 1);
         CRYSTAL2 = Math.floor(Math.random() * 12 + 1);
@@ -97,7 +113,5 @@ function reset() {
 
         $("#random-number").text(RANDOMNUMBER);
         $("#your-score").text(SCORE);
-        $("#wins").text(WINS);
-        $("#losses").text(LOSSES);
     })
 }
